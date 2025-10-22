32b6de20b2a777871e07913ee486105f2d78aa73
Shamsul <48136141+shkms11@users.noreply.github.com>
2025-10-22 07:36:53 +0600

    boilerplate

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProductCatalogBackend.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
